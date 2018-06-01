a3a5b06038551f701ec57f35bec5c32746fcc7c2
Rodrigo Richter <guigorichter@gmail.com>
2018-06-01 15:09:49 -0300

    Revert "iniciado interface"
    This reverts commit b2597a814c87f8d16fbba0416bcaef163f4e968f.

--- soundmixer/soundmixer/mainWindow.cpp
+++ soundmixer/soundmixer/mainWindow.cpp
@@ -2,68 +2,39 @@
 #include "Mixer.h"
 #include "Serial.h"
 #include "App.h"
-#include "atlconv.h"
-#include <string>
 
 #include <iostream>
-
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-
 int main()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
+
 	soundmixer::mainWindow form;
 	Mixer m;
-	/*char input[10] = "";
-	cout << "qual a com port? Escrever COM1, COM2, etc...: ";
-	cin >> input;
-	Serial s(input);*/
-	char buffer[20];
-	TCHAR processName[MAX_PATH] = TEXT("Empty.exe");
-	while (0) {
-		/*strcpy(buffer, "empt");
+	/*Serial s("COM3");
+	char buffer[10];
+	while (1) {
+		strcpy(buffer, "empt");
 		s.readSerial(buffer, 4);
 		buffer[4] = '\0';
 		if (!strcmp(buffer, "tmut")) {
-			if (m.GetMuteMaster() == true)
-				m.SetMuteMaster(false);
-			else if (m.GetMuteMaster() == false)
-				m.SetMuteMaster(true);
+			if (m.GetMuteMasterVolume() == true)
+				m.SetMuteMasterVolume(false);
+			else if (m.GetMuteMasterVolume() == false)
+				m.SetMuteMasterVolume(true);
 		}
-
-		if (!strcmp(buffer, "svap")) {
-			s.readSerial(buffer, 20);
-			_tcscpy(processName, (TCHAR*)buffer);
-			s.readSerial(buffer, 1);
-			float v = buffer[0];
-			 m.SetApplicationVolume(processName, v);
-		}
-
-		if (!strcmp(buffer, "tmap")) {
-			s.readSerial(buffer, 20);
-			_tcscpy(processName, (TCHAR*)buffer);
-			s.readSerial(buffer, 1);
-			float v = buffer[0];
-			
-			if (m.GetMuteApplication(processName) == true)
-				m.SetMuteApplication(processName, false);
-			else if (m.GetMuteApplication(processName) == false)
-				m.SetMuteApplication(processName, true);
-		}*/
-	}
+	}*/
 	double vol = 0.25;
 	//m.SetMasterVolume(vol, true);
-	std::cout << m.SetMuteApplication(processName,true);
+	TCHAR processName[MAX_PATH] = TEXT("Steam.exe");
+
+	std::cout << m.SetMuteApplication(processName,1);
 
 	Application::Run(%form);
 }
 
-void soundmixer::mainWindow::setComPort(std::string inputPort)
-{
-	strcpy(COMPort, inputPort.c_str());
-}
--- soundmixer/soundmixer/mainWindow.h
+++ soundmixer/soundmixer/mainWindow.h
@@ -1,14 +1,4 @@
 #pragma once
-#include "Mixer.h"
-#include "Serial.h"
-#include "App.h"
-#include "atlconv.h"
-#include <string>
-#include <msclr\marshal_cppstd.h>
-#include <iostream>
-
-char COMPort[32];
-Serial *serialCon;
 
 namespace soundmixer {
 
@@ -32,7 +22,6 @@ namespace soundmixer {
 			//TODO: Add the constructor code here
 			//
 		}
-		void setComPort(std::string inputPort);
 
 	protected:
 		/// <summary>
@@ -46,13 +35,6 @@ namespace soundmixer {
 			}
 		}
 	private: System::Windows::Forms::Label^  label1;
-	protected:
-	private: System::Windows::Forms::ComboBox^  comboBox1;
-	private: System::Windows::Forms::Button^  button1;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::ComboBox^  comboBox2;
-
-
 	protected:
 
 	private:
@@ -69,71 +51,23 @@ namespace soundmixer {
 		void InitializeComponent(void)
 		{
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(9, 9);
+			this->label1->Location = System::Drawing::Point(177, 89);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(59, 13);
+			this->label1->Size = System::Drawing::Size(33, 13);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Application";
+			this->label1->Text = L"dalee";
 			this->label1->Click += gcnew System::EventHandler(this, &mainWindow::label1_Click);
-			// 
-			// comboBox1
-			// 
-			this->comboBox1->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
-			this->comboBox1->FormattingEnabled = true;
-			this->comboBox1->Location = System::Drawing::Point(12, 25);
-			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->RightToLeft = System::Windows::Forms::RightToLeft::No;
-			this->comboBox1->Size = System::Drawing::Size(121, 21);
-			this->comboBox1->TabIndex = 4;
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(12, 52);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 3;
-			this->button1->Text = L"Refresh List";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &mainWindow::button1_Click);
-			// 
-			// label2
-			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(9, 92);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(53, 13);
-			this->label2->TabIndex = 5;
-			this->label2->Text = L"COM Port";
-			// 
-			// comboBox2
-			// 
-			this->comboBox2->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
-			this->comboBox2->FormattingEnabled = true;
-			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(6) { L"COM1", L"COM2", L"COM3", L"COM4", L"COM5", L"COM6" });
-			this->comboBox2->Location = System::Drawing::Point(12, 108);
-			this->comboBox2->Name = L"comboBox2";
-			this->comboBox2->Size = System::Drawing::Size(121, 21);
-			this->comboBox2->TabIndex = 6;
-			this->comboBox2->SelectedIndexChanged += gcnew System::EventHandler(this, &mainWindow::comboBox2_SelectedIndexChanged);
 			// 
 			// mainWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(432, 348);
-			this->Controls->Add(this->comboBox2);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->comboBox1);
-			this->Controls->Add(this->button1);
+			this->ClientSize = System::Drawing::Size(437, 348);
 			this->Controls->Add(this->label1);
 			this->Name = L"mainWindow";
 			this->Text = L"Soundmixer";
@@ -144,14 +78,5 @@ namespace soundmixer {
 #pragma endregion
 	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
 	}
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-		cout << "eae" << endl;
-	}
-	private: System::Void comboBox2_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
-		Object^ selectedItem = comboBox2->SelectedItem;
-		System::String^ s = selectedItem->ToString();
-		setComPort(msclr::interop::marshal_as<std::string>(s));
-		serialCon = new Serial(COMPort);
-	}
-};
+	};
 }
