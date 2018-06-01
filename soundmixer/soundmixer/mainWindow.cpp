c61ae156457893d097d92e3419fe021fb0666778
Rodrigo Richter <guigorichter@gmail.com>
2018-06-01 15:12:51 -0300

    Revert "Revert "iniciado interface""
    This reverts commit a3a5b06038551f701ec57f35bec5c32746fcc7c2.

--- soundmixer/soundmixer/mainWindow.cpp
+++ soundmixer/soundmixer/mainWindow.cpp
@@ -2,39 +2,68 @@
 #include "Mixer.h"
 #include "Serial.h"
 #include "App.h"
+#include "atlconv.h"
+#include <string>
 
 #include <iostream>
+
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
+
 int main()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-
 	soundmixer::mainWindow form;
 	Mixer m;
-	/*Serial s("COM3");
-	char buffer[10];
-	while (1) {
-		strcpy(buffer, "empt");
+	/*char input[10] = "";
+	cout << "qual a com port? Escrever COM1, COM2, etc...: ";
+	cin >> input;
+	Serial s(input);*/
+	char buffer[20];
+	TCHAR processName[MAX_PATH] = TEXT("Empty.exe");
+	while (0) {
+		/*strcpy(buffer, "empt");
 		s.readSerial(buffer, 4);
 		buffer[4] = '\0';
 		if (!strcmp(buffer, "tmut")) {
-			if (m.GetMuteMasterVolume() == true)
-				m.SetMuteMasterVolume(false);
-			else if (m.GetMuteMasterVolume() == false)
-				m.SetMuteMasterVolume(true);
+			if (m.GetMuteMaster() == true)
+				m.SetMuteMaster(false);
+			else if (m.GetMuteMaster() == false)
+				m.SetMuteMaster(true);
 		}
-	}*/
+
+		if (!strcmp(buffer, "svap")) {
+			s.readSerial(buffer, 20);
+			_tcscpy(processName, (TCHAR*)buffer);
+			s.readSerial(buffer, 1);
+			float v = buffer[0];
+			 m.SetApplicationVolume(processName, v);
+		}
+
+		if (!strcmp(buffer, "tmap")) {
+			s.readSerial(buffer, 20);
+			_tcscpy(processName, (TCHAR*)buffer);
+			s.readSerial(buffer, 1);
+			float v = buffer[0];
+			
+			if (m.GetMuteApplication(processName) == true)
+				m.SetMuteApplication(processName, false);
+			else if (m.GetMuteApplication(processName) == false)
+				m.SetMuteApplication(processName, true);
+		}*/
+	}
 	double vol = 0.25;
 	//m.SetMasterVolume(vol, true);
-	TCHAR processName[MAX_PATH] = TEXT("Steam.exe");
-
-	std::cout << m.SetMuteApplication(processName,1);
+	std::cout << m.SetMuteApplication(processName,true);
 
 	Application::Run(%form);
 }
 
+void soundmixer::mainWindow::setComPort(std::string inputPort)
+{
+	strcpy(COMPort, inputPort.c_str());
+}
